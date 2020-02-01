9a457c6f7a994a1207278775279d5bf617ba3b0e
Pankaj Sherchan <pshercha@berkadia.com>
2020-02-01 11:58:41 -0500

    added palette to the project

--- network-overflow/Patterns and Best Practices.md
+++ network-overflow/Patterns and Best Practices.md
@@ -6,16 +6,20 @@
 
 [React Style Guide](https://github.com/airbnb/javascript/tree/master/react)
 
-- Essential extensions: (necessary for consistency and code quality)
-- TSLint
-- EditorConfig for VS Code
-- Prettier - Code formatter
-- Code Spell Checker
+## Visual Studio Code SETUP
 
-- Settings:
+    - Essential extensions: (necessary for consistency and code quality)
+    - TSLint
+    - EditorConfig for VS Code
+    - Prettier - Code formatter
+    - Code Spell Checker
 
-- TypeScript › Preferences: Import Module Specifier - set this to "relative" (ensures that all suggested imports are relative paths)
+    - Settings:
 
-- Editor: Format On Save - make sure box is checked ( Ensures that formatting from above extensions is applied whenever a file is saved)
+    - TypeScript › Preferences: Import Module Specifier - set this to "relative" (ensures that all suggested imports are relative paths)
 
-# Lets see
+    - Editor: Format On Save - make sure box is checked ( Ensures that formatting from above extensions is applied whenever a file is saved)
+
+# Commits
+
+    - use AB#1234 where 1234 being task or PBI number
--- network-overflow/src/App.js
+++ network-overflow/src/App.js
@@ -1,27 +1,84 @@
-import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  TextField,
+  Toolbar
+} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import { ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import "./App.css";
+import theme from "./theme";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  buttonMargin: {
+    margin: theme.spacing(2)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to adfd reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+
+      <div className={classes.buttonMargin}>
+        <TextField
+          error
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+
+      <div className={classes.buttonMargin}>
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" disabled>
+          Disabled
+        </Button>
+        <Button variant="contained" color="primary" href="#contained-buttons">
+          Link
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
