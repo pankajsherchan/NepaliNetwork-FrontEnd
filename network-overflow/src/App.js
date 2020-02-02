f619a993bafe5fc52d5967229a91dc88be25e0e4
Pankaj Sherchan <pshercha@berkadia.com>
2020-02-02 11:02:03 -0500

    AB#32 react routes configured

--- network-overflow/src/App.js
+++ network-overflow/src/App.js
@@ -1,83 +1,18 @@
-import {
-  AppBar,
-  Button,
-  makeStyles,
-  TextField,
-  Toolbar
-} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-import { ThemeProvider } from "@material-ui/styles";
-import React from "react";
-import "./App.css";
-import theme from "./theme";
+import { ThemeProvider } from '@material-ui/styles';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './Routes';
+import theme from './theme';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  },
-  buttonMargin: {
-    margin: theme.spacing(2)
-  }
-}));
+const browserHistory = createBrowserHistory();
 
 function App() {
-  const classes = useStyles();
-
   return (
     <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-
-      <div className={classes.buttonMargin}>
-        <TextField
-          error
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-        />
-
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div>
-
-      <div className={classes.buttonMargin}>
-        <Button variant="contained">Default</Button>
-        <Button variant="contained" color="primary">
-          Primary
-        </Button>
-        <Button variant="contained" color="secondary">
-          Secondary
-        </Button>
-        <Button variant="contained" disabled>
-          Disabled
-        </Button>
-        <Button variant="contained" color="primary" href="#contained-buttons">
-          Link
-        </Button>
-      </div>
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
     </ThemeProvider>
   );
 }
