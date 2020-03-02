32c2d345044abb46e8c4f50658de0a99e49954cf
amanandhar2 <amanandhar2@dxc.com>
2020-03-02 16:51:09 -0600

    Basic FrontEnd-Backend Connect
    Connect Apis and front end
    Simple dialog boxes

--- network-overflow/src/views/signin/SignIn.js
+++ network-overflow/src/views/signin/SignIn.js
@@ -1,25 +1,28 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import axios from "axios";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import AuthContext from '../../context/auth-context';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import SimpleDialog from '../../shared/components/dialog/SimpleDialog';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link to="/">NetworkOverflow </Link> {new Date().getFullYear()}
-      {"."}
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link to='/'>NetworkOverflow </Link> {new Date().getFullYear()}
+      {'.'}
     </Typography>
   );
 }
@@ -27,93 +30,194 @@ function Copyright() {
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    color: "black"
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'black'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: "red"
+    backgroundColor: 'red'
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
-    color: "black"
+    color: 'black'
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
+  },
+  multilineColor: {
+    color: 'black'
   }
 }));
 
-export default function SignIn() {
+const SignIn = () => {
   const classes = useStyles();
 
-  function signIn(event) {
-    const data = new FormData(event.target);
-    console.log(data);
-    this.props.history.push("/signup");
+  const [showDialog, setShowDialog] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState('');
+  const [dialogTitle, setDialogTitle] = useState('');
 
-    // axios
-    //   .post("http://localhost:3000/signin", { email: email, password: password })
-    //   .then(res => {
-    //     if (res == true) {
-    //     }
-    //   });
-  }
+  const validate = values => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length < 6) {
+      errors.password = 'Password must be at least 6 character long';
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validate,
+    onSubmit: async user => {
+      login(user);
+    }
+  });
+
+  const hideDialogBox = () => {
+    setShowDialog(false);
+  };
+
+  const showDialogBox = (title, message) => {
+    setShowDialog(true);
+    setDialogMessage(message);
+    setDialogTitle(title);
+  };
+
+  const context = useContext(AuthContext);
+
+  const login = async user => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      await axios
+        .post(`http://localhost:5000/api/login`, user, config)
+        .then(res => {
+          if (res.data.message) {
+            return showDialogBox('Warning', res.data.message);
+          }
+          const token = 'Bearer ' + res.data.result.token;
+          localStorage.setItem('token', token);
+          context.setIsLoggedIn(true);
+          showDialogBox('Success', 'Logged in successfully');
+        })
+        .catch(error => {});
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
+      <div>
+        {showDialog ? (
+          <SimpleDialog
+            open={showDialog}
+            message={dialogMessage}
+            title={dialogTitle}
+            hide={hideDialogBox}
+          ></SimpleDialog>
+        ) : null}
+      </div>
+
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component='h1' variant='h5'>
           Sign in
         </Typography>
-        <form className={classes.form} noValidate onSubmit={signIn}>
+        <form
+          onSubmit={formik.handleSubmit}
+          className={classes.form}
+          noValidate
+        >
           <TextField
-            margin="normal"
+            variant='outlined'
+            margin='normal'
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            multiline
+            InputProps={{
+              className: classes.multilineColor
+            }}
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
             autoFocus
-            style={{ color: "black" }}
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
+
+          {formik.errors.email ? (
+            <div className='form-error'>{formik.errors.email}</div>
+          ) : null}
+
           <TextField
-            margin="normal"
-            color="black"
+            variant='outlined'
+            margin='normal'
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
             required
             fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
+            InputProps={{
+              className: classes.multilineColor
+            }}
+            value={formik.values.password}
+            onChange={formik.handleChange}
           />
+
+          {formik.errors.password ? (
+            <div className='form-error'>{formik.errors.password}</div>
+          ) : null}
+
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
           />
           <Button
-            type="submit"
+            type='submit'
             fullWidth
-            variant="contained"
-            color="primary"
+            variant='contained'
+            color='primary'
             className={classes.submit}
           >
             Sign In
           </Button>
           <Grid container>
             <Grid item xs>
-              {/* <Link>Forgot password?</Link> */}
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
             </Grid>
             <Grid item>
-              <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
+              <Link to='/signUp' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </Link>
             </Grid>
           </Grid>
         </form>
@@ -123,4 +227,6 @@ export default function SignIn() {
       </Box>
     </Container>
   );
-}
+};
+
+export default SignIn;
